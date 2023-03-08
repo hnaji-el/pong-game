4299b041552116dc7cc6f511ee20a4495f737d3b
ayoub <ayoubafdel@gmail.com>
2023-03-08 17:48:07 +0100

    queue gateway logic

--- game/src/game/game.service.ts
+++ game/src/game/game.service.ts
@@ -7,5 +7,17 @@ export class GameService{
 
     addToQueue(player: Socket):void {
         console.log("hello from GameService");
+        this.players.push(player);
+        if (this.players.length >= 2){
+            const player1 = this.players.shift();
+            const player2 = this.players.shift();
+            if (player1 && player2){
+                player1.emit('launchGame');
+                player2.emit('launchGame');
+            }
+        }
+        else{
+            player.emit('waitingInQueue');
+        }
     }
 }

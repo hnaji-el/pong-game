37d6a3c7fc4c0282fbc753592291a5120eb3883a
MustaphaOuassit <mustaphaouassit60@gmail.com>
2023-03-19 15:56:02 +0000

    add: spinner

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./Components/Routes/Home"
 import Messages from "./Components/Routes/Messages"
 import Profile from "./Components/Routes/Profile"
 import ProfileUser from "./Components/Routes/ProfileUser"
+import Redirection from "./Components/Routes/Redirection"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Redirection" element={<Redirection />} />
         <Route path="/Messages" element={<Messages />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/ProfileUser" element={<ProfileUser />} />
--- src/Components/Routes/Home.tsx
+++ src/Components/Routes/Home.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect,createContext } from "react";
+import React, { useEffect, createContext } from "react";
 import Navigation from "../Navigation/Navigation";
-import fire from "../../assets/fire.png"
+import fire from "../../assets/fire.png";
 
-export const ActiveHome = createContext<boolean>(false)
+export const ActiveHome = createContext<boolean>(false);
 
 export default function Home() {
   useEffect(() => {
     document.title = "Pong - Home";
   }, []);
 
-  return(
+  return (
     <ActiveHome.Provider value={true}>
       <Navigation />
+      {/* <div className="mx-3 pb-20 lg:pb-0 lg:ml-64 lg:mr-4 flex justify-center items-center h-full">
+        <Spinner />
+      </div> */}
       <main className="mx-3 pb-20 lg:pb-1 pt-10 lg:ml-64 lg:mr-4">
       <div className='flex flex-col gap-5 w-full h-full'>
         <h1 className='text-primaryText text-2xl flex items-center gap-1.5'><span>Live Games</span><img src={fire} alt="fire" className='w-4' /></h1>

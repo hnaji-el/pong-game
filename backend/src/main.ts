90d1827840548586d92c6f35828c4fbbd1a4a74a
hnaji-el <hamidnaji981@gmail.com>
2023-03-27 22:28:44 +0000

    add docker compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(5000);
 }
 
 bootstrap();

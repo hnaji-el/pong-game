2edecaa97dceeec27d45329371c5602da6b6e63d
hnaji-el <hamidnaji981@gmail.com>
2024-10-10 19:36:57 +0100

    changed the logic of username generation

--- backend/src/auth/google.strategy.ts
+++ backend/src/auth/google.strategy.ts
@@ -22,8 +22,12 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
-    const { given_name, family_name, email, picture } = profile._json;
-    const username = this.authService.getUsername(given_name, family_name);
+    // TODO: generate an elegante username & don't make it unique
+
+    // const { given_name, family_name, email, picture } = profile._json;
+    // const username = this.authService.getUsername(given_name, family_name);
+    const { email, picture } = profile._json;
+    const username = email.slice(0, email.indexOf('@'));
 
     const user = await this.authService.validateUser(username, email, picture);
 

cb58dc9036cd323374f51584bb1309514bcc1d7d
hnaji-el <hamidnaji981@gmail.com>
2024-12-21 19:12:20 +0100

    Optimized the returned data for logged user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -21,13 +21,16 @@ export class UsersController {
 
   @Get('/users/logged-user')
   @UseGuards(JwtAuthGuard)
-  getLoggedUser(@Req() req) {
+  getLoggedUser(@Req() req: Request) {
+    delete req.user.requester;
+    delete req.user.addressee;
+
     return req.user;
   }
 
   @Patch('/users/update_nickname')
   @UseGuards(JwtAuthGuard)
-  async updateNickname(@Req() req, @Body() body) {
+  async updateNickname(@Req() req: Request, @Body() body) {
     await this.usersService.updateNickname(req.user.id, body.nickname);
   }
 
@@ -57,7 +60,7 @@ export class UsersController {
     }),
   )
   async uploadPictureAndPassValidation(
-    @Req() req,
+    @Req() req: Request,
     @UploadedFile(new PictureValidatorPipe()) file: Express.Multer.File,
   ) {
     return await this.usersService.updateUserPictureURL(req.user, file);
@@ -71,7 +74,7 @@ export class UsersController {
 
   @Get('/users/:id')
   @UseGuards(JwtAuthGuard)
-  async getOneUser(@Req() req, @Param('id') id: string) {
+  async getOneUser(@Req() req: Request, @Param('id') id: string) {
     return await this.usersService.getOneUser(req.user, id);
   }
 
@@ -83,25 +86,25 @@ export class UsersController {
 
   @Post('/users/add-friend/:id')
   @UseGuards(JwtAuthGuard)
-  async addFriend(@Req() req, @Param('id') id: string) {
+  async addFriend(@Req() req: Request, @Param('id') id: string) {
     await this.usersService.addFriend(req.user, id);
   }
 
   @Delete('/users/remove-friend/:id')
   @UseGuards(JwtAuthGuard)
-  async removeFriend(@Req() req, @Param('id') id: string) {
+  async removeFriend(@Req() req: Request, @Param('id') id: string) {
     await this.usersService.removeFriend(req.user, id);
   }
 
   @Patch('/users/block-friend/:id')
   @UseGuards(JwtAuthGuard)
-  async blockUser(@Req() req, @Param('id') id: string) {
+  async blockUser(@Req() req: Request, @Param('id') id: string) {
     await this.usersService.blockUser(req.user, id);
   }
 
   @Patch('/users/unblock-friend/:id')
   @UseGuards(JwtAuthGuard)
-  async unblockUser(@Req() req, @Param('id') id: string) {
+  async unblockUser(@Req() req: Request, @Param('id') id: string) {
     await this.usersService.unblockUser(req.user, id);
   }
 
